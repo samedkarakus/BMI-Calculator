2d4b9a37fdc406b888642f44451567cfeef7679a
samedkarakus <samedkarakus.ceng@gmail.com>
2024-07-29 01:19:25 +0300

    methods added.

---
+++ BMI Calculator/Controller/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Samed Karakuş on 29.07.2024.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- BMI Calculator/Controller/ViewController.swift
+++ BMI Calculator/Controller/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var height: UILabel!
+    @IBOutlet weak var weight: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightChanged(_ sender: UISlider) {
+        let heightValue = String(Int(sender.value))
+        height.text = "\(heightValue) cm"
+    }
+    
+    @IBAction func weightChanged(_ sender: UISlider) {
+        let weightValue = String(format: "%.2f", roundNearestHalf(sender.value))
+        weight.text = "\(weightValue) kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        print(Int(heightSlider.value))
+        print(String(format: "%.2f", roundNearestHalf(weightSlider.value)))
+        print(BMICalculation(heightSlider.value, weightSlider.value))
+    }
+    
+    func BMICalculation(_ height: Float, _ weight: Float) -> String {
+        let heightInMeters = height / 100
+        let bmi = weight / (heightInMeters * heightInMeters)
+        return String(format: "%.1f", bmi)
+    }
+    
+    func roundNearestHalf(_ value: Float) -> Float {
+        return round(value * 2.0) / 2.0
+    }
 }
-
